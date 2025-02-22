f56604ef406eeff23b3ceccb0bc7bf02b2147fea
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-02-22 11:48:34 +0100

    Update bash.ts

--- src/core/bash/bash.ts
+++ src/core/bash/bash.ts
@@ -35,6 +35,10 @@ import process from 'process';
 function niceBytes(a: Number) { let b = 0, c = parseInt((a.toString()), 10) || 0; for (; 1024 <= c && ++b;)c /= 1024; return c.toFixed(10 > c && 0 < b ? 1 : 0) + " " + ["bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"][b] }
 
 export default async (client: Client) => {
+    if (!client.config.core.bash) {
+        return;
+    }
+
     const PORT = 3871;
 
     const user = {
